df9bed812d0a103f4fe02442dc2acf58bc9c65e4
Arturo <123048139@upq.edu.mx>
2026-07-02 18:06:33 -0600

    Configuración inicial del proyecto

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import MenuScreen from "./screens/MenuScreen";
+import TarjetasScreen from "./screens/TarjetasScreen";
+import SafeAreaScreen from "./screens/SafeAreaScreen";
+import PressableScreen from "./screens/PressableScreen";
+import TextInputScreen from "./screens/TextInputScreen";
+import ActivityIndicatorScreen from "./screens/ActivityIndicatorScreen";
+import ImageBackgroundScreen from "./screens/ImageBackgroundScreen";
+import FlatListSectionList from "./screens/FlatListSectionList";
+import ModalScreen from "./screens/ModalScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Menu">
+        <Stack.Screen
+          name="Menu"
+          component={MenuScreen}
+          options={{ title: "Componentes React Native" }}
+        />
+        <Stack.Screen name="Tarjetas" component={TarjetasScreen} />
+        <Stack.Screen name="SafeArea" component={SafeAreaScreen} />
+        <Stack.Screen name="Pressable" component={PressableScreen} />
+        <Stack.Screen name="TextInput" component={TextInputScreen} />
+        <Stack.Screen
+          name="ActivityIndicator"
+          component={ActivityIndicatorScreen}
+        />
+        <Stack.Screen
+          name="ImageBackground"
+          component={ImageBackgroundScreen}
+        />
+        <Stack.Screen name="FlatList" component={FlatListSectionList} />
+        <Stack.Screen name="Modal" component={ModalScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
